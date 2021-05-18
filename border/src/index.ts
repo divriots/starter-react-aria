7a125db831196a09583d05295472417a49fb24e5
Gregory Quinio <quinio.gregory@gmail.com>
2021-05-18 07:49:58 +0000

    init

---
+++ border/src/index.ts
@@ -0,0 +1,8 @@
+export const border = {
+  borderWidths: [0, '1px', '4px'],
+  borderStyles: ['solid', 'dotted', 'dashed'],
+  borders: {
+    thinBlack: '1px solid #000',
+    thickBlack: '4px solid #000',
+  },
+};
