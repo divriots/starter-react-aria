23d3090e7f814994f10fbe27b1682759ef1599d8
mveres <veres85@yahoo.com>
2021-05-20 22:37:52 +0300

    removed adobe logo

--- layout/src/mdx-layout.tsx
+++ layout/src/mdx-layout.tsx
@@ -5,24 +5,7 @@ import { CoreLayout } from '@divriots/dockit-react/mdx-layout-core';
 export const Layout = (props) => (
   <MDXProvider>
     <CoreLayout
-      logo={
-        <>
-          <svg
-            viewBox="0 0 30 26"
-            fill="#E1251B"
-            aria-label="Adobe"
-            style={{
-              width: '36px',
-              height: '32px',
-              display: 'block',
-              marginRight: '16px',
-            }}>
-            <polygon points="19,0 30,0 30,26"></polygon>
-            <polygon points="11.1,0 0,0 0,26"></polygon>
-            <polygon points="15,9.6 22.1,26 17.5,26 15.4,20.8 10.2,20.8"></polygon>
-          </svg>
-        </>
-      }
+      logo={<h3 style={{ fontWeight: 700 }}>React Aria</h3>}
       {...props} />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css" />
   </MDXProvider>
