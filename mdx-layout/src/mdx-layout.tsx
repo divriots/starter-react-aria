7415204c63e36fe73642f2e49de6d1237d5d5292
m4dz <code@m4dz.net>
2021-10-19 13:41:45 +0000

    Add logo header

--- mdx-layout/src/mdx-layout.tsx
+++ mdx-layout/src/mdx-layout.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { CoreLayout } from '@divriots/dockit-react/mdx-layout-core';
+import styles from './corelayout.module.scss';
+
 import '~/all/src/all.scss';
 
 export const Layout = (props) => (
   <MDXProvider>
     <CoreLayout
-      logo={<h3 style={{ fontWeight: 700 }}>React Aria</h3>}
+      logo={
+        <>
+          <img
+            className={`${styles.logo}`}
+            src="https://www.adobe.com/content/dam/cc/icons/Adobe_Corporate_Horizontal_Red_HEX.svg"
+          />
+          <h3 className={`${styles.heading}`} style={{ fontWeight: 700 }}>
+            React Aria
+          </h3>
+        </>
+      }
       {...props}
     />
     <link
