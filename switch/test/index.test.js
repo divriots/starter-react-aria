b8804611fa49298d802906648121152ae7c6694b
Georges Gomes <5250572+undefined@users.noreply.github.com>
2022-05-09 16:47:13 +0000

    Push from demo-p

---
+++ switch/test/index.test.js
@@ -0,0 +1,8 @@
+import chai from 'chai/chai.js';
+const expect = chai.expect;
+
+describe('Dummy test', function () {
+  it('should be always true', function () {
+    expect(true).to.be.false;
+  });
+});
